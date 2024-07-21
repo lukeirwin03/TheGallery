d38f098984b031a67bae6600bf6eb4b55bdcf5f9
Luke Irwin <lukemirwin@gmail.com>
2024-07-21 13:00:19 -0500

    updating the assets

--- src/main.js
+++ src/main.js
@@ -4,64 +4,68 @@ import world from "./scenes/world.js";
 import gallery from "./scenes/gallery.js";
 
 k.loadFont("gameboy", "./assets/gb.ttf");
-k.loadSprite("assets", "./assets/maps/new_gallery_sprites.png", {
+k.loadSprite("assets", "./assets/maps/sprites/src/new_gallery_sprites.png", {
   sliceX: 16,
   sliceY: 16,
   anims: {
-    // "player-idle-down": 209,
-    // "player-down": {
-    //   from: 210,
-    //   to: 212,
-    //   loop: true,
-    // },
-    // "player-idle-side": 225,
-    // "player-side": {
-    //   from: 226,
-    //   to: 229,
-    //   loop: true,
-    // },
-    // "player-idle-up": 241,
-    // "player-up": {
-    //   from: 242,
-    //   to: 244,
-    //   loop: true,
-    // },
+    "player-idle-down": 119,
     "player-down": {
-      from: 224,
-      to: 225,
+      from: 117,
+      to: 118,
+      loop: true,
+      speed:5,
+    },
+    "player-idle-side": 132,
+    "player-side": {
+      from: 133,
+      to: 137,
       loop: true,
       speed:6,
-  },
-    "player-idle-down": {
-      from: 224,
-      to: 225,
+    },
+    "player-idle-up": 103,
+    "player-up": {
+      from: 101,
+      to: 102,
       loop: true,
-      speed:6,
-  },
-  "player-side": {
-      from: 226,
-      to: 227,
-      loop: true,
-      speed:6,
-  },
-  "player-idle-side": {
-      from: 226,
-      to: 227,
-      loop: true,
-      speed:6,
-  },
-  "player-up": {
-      from: 240,
-      to: 241,
-      loop: true,
-      speed:6,
-  },
-  "player-idle-up": {
-      from: 240,
-      to: 241,
-      loop: true,
-      speed:6,
-  },
+      speed:5,
+
+    },
+  //   "player-down": {
+  //     from: 224,
+  //     to: 225,
+  //     loop: true,
+  //     speed:6,
+  // },
+  //   "player-idle-down": {
+  //     from: 224,
+  //     to: 225,
+  //     loop: true,s
+  //     speed:6,
+  // },
+  // "player-side": {
+  //     from: 226,
+  //     to: 227,
+  //     loop: true,
+  //     speed:6,
+  // },
+  // "player-idle-side": {
+  //     from: 226,
+  //     to: 227,
+  //     loop: true,
+  //     speed:6,
+  // },
+  // "player-up": {
+  //     from: 240,
+  //     to: 241,
+  //     loop: true,
+  //     speed:6,
+  // },
+  // "player-idle-up": {
+  //     from: 240,
+  //     to: 241,
+  //     loop: true,
+  //     speed:6,
+  // },
     "guide-idle-down": {
         from: 224,
         to: 225,
--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -5,43 +5,54 @@ import { colorizeBackground } from "../utils.js";
 export default function mainMenu(k) {
   const currentLanguage = gameState.getLanguage();
 
-  colorizeBackground(k, 0, 0, 0);
+  // colorizeBackground(k, 0, 0, 0);
+  k.loadSprite("background", "../../assets/maps/gallery_main_menu.png");
 
-  k.add([
-    k.text(menuText[currentLanguage].title, { size: 56, font: "gameboy" }),
-    k.area(),
-    k.anchor("center"),
-    k.pos(k.center().x, k.center().y - 100),
-  ]);
+  k.scene("mainMenu", () => {
+    k.add([
+      k.sprite("background", { width: k.width(), height: k.height() }), // Adjust to fit the screen
+      k.pos(0, 0),
+    ]);
 
-  k.add([
-    k.text(menuText[currentLanguage].languageIndication, {
-      size: 14,
-      font: "gameboy",
-    }),
-    k.area(),
-    k.anchor("center"),
-    k.pos(k.center().x, k.center().y + 75),
-  ]);
+    k.add([
+      k.text(menuText[currentLanguage].title, { size: 56, font: "gameboy" }),
+      k.area(),
+      k.anchor("center"),
+      k.pos(k.center().x, k.center().y - 100),
+    ]);
 
-  k.add([
-    k.text(menuText[currentLanguage].playIndication, {
-      size: 24,
-      font: "gameboy",
-    }),
-    k.area(),
-    k.anchor("center"),
-    k.pos(k.center().x, k.center().y + 155),
-  ]);
+    k.add([
+      k.text(menuText[currentLanguage].languageIndication, {
+        size: 14,
+        font: "gameboy",
+      }),
+      k.area(),
+      k.anchor("center"),
+      k.pos(k.center().x, k.center().y + 75),
+    ]);
 
-  k.onKeyPress("f", () => {
-    if (currentLanguage !== "spanish") gameState.setLanguage("spanish");
-    if (currentLanguage !== "english") gameState.setLanguage("english");
-    k.go("mainMenu");
+    k.add([
+      k.text(menuText[currentLanguage].playIndication, {
+        size: 24,
+        font: "gameboy",
+      }),
+      k.area(),
+      k.anchor("center"),
+      k.pos(k.center().x, k.center().y + 155),
+    ]);
+
+    k.onKeyPress("f", () => {
+      if (currentLanguage !== "spanish") gameState.setLanguage("spanish");
+      if (currentLanguage !== "english") gameState.setLanguage("english");
+      k.go("mainMenu");
+    });
+
+    k.onKeyPress("enter", () => {
+      if (gameState.getLanguage() === "spanish") gameState.setFontSize(28);
+      k.go("world");
+    });
   });
 
-  k.onKeyPress("enter", () => {
-    if (gameState.getLanguage() === "spanish") gameState.setFontSize(28);
-    k.go("world");
-  });
+  // Start the main menu scene
+  k.go("mainMenu");
 }
